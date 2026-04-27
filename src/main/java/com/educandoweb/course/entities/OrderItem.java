2c02dba97ec42f44a088d4f3f908d2deda0e0773
Bruno José Vilas Bôas <bruuno28.28@gmail.com>
2026-04-27 10:36:27 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import com.educandoweb.course.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
@@ -12,74 +13,78 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_order_item")
 public class OrderItem implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-	
-	@EmbeddedId
-	private OrderItemPK id;
-	private Integer quantity;
-	private Double price;
-	
-	public OrderItem() {
-		
-	}
 
-	public OrderItem(Order order, Product product, Integer quantity, Double price) {
-		super();
-		id.setOrder(order);
-		id.setProduct(product);
-		this.quantity = quantity;
-		this.price = price;
-	}
-	
-	public Order getOrder() {
-		return id.getOrder();
-	}
-	
-	public void setOrder(Order order) {
-		id.setOrder(order);
-	}
-	public Product getProduct() {
-		return id.getProduct();
-	}
-	
-	public void setProduct(Product product) {
-		id.setProduct(product);
-	}
+    private static final long serialVersionUID = 1L;
 
-	public Integer getQuantity() {
-		return quantity;
-	}
+    @EmbeddedId
+    private OrderItemPK id = new OrderItemPK();
 
-	public void setQuantity(Integer quantity) {
-		this.quantity = quantity;
-	}
+    private Integer quantity;
+    private Double price;
 
-	public Double getPrice() {
-		return price;
-	}
+    public OrderItem() {
+    }
 
-	public void setPrice(Double price) {
-		this.price = price;
-	}
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        super();
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
+    @JsonIgnore
+    public Order getOrder() {
+        return id.getOrder();
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		OrderItem other = (OrderItem) obj;
-		return Objects.equals(id, other.id);
-	}
-	
-	
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
 
+    public Product getProduct() {
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        id.setProduct(product);
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    // Método muito importante que o professor usa:
+    public Double getSubTotal() {
+        return price * quantity;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        OrderItem other = (OrderItem) obj;
+        return Objects.equals(id, other.id);
+    }
 }
