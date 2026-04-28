f7999ff567988b749841a60c895d25107b7cb8f1
Bruno José Vilas Bôas <bruuno28.28@gmail.com>
2026-04-28 08:55:16 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -17,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -40,6 +41,9 @@ public class Order implements Serializable {
 
     @OneToMany(mappedBy = "id.order", cascade = CascadeType.ALL)
     private Set<OrderItem> items = new HashSet<>();
+    
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order() {
     }
@@ -88,8 +92,16 @@ public class Order implements Serializable {
         }
         return sum;
     }
+ 
+    public Payment getPayment() {
+		return payment;
+	}
 
-    @Override
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	@Override
     public int hashCode() {
         return Objects.hash(id);
     }
